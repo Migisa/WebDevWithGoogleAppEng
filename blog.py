a708101be90d3158e8eb4ebee479fb7c20706d7a
Alex Iljina <alexandra.iljina@gmail.com>
2013-02-23 16:57:13 +0000

    Finished unit 3 homework,blog with the backend

--- blog.py
+++ blog.py
@@ -2,11 +2,17 @@ import os
 import webapp2
 import jinja2
 from google.appengine.ext import db
+import re
 
 
 # put jinja_env here
 template_dir = os.path.join(os.path.dirname(__file__),"templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+
+def blog_key(name='default'):
+	return db.Key.from_path('blogs', name)
 
 
 
@@ -28,13 +34,18 @@ class Posts(db.Model):
 	subject = db.StringProperty(required = True)
 	content = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True)
+	last_modified = db.DateTimeProperty(auto_now = True)
 
+	def render(self):
+		self.render_text = self.content.replace('\n', '<br>')
+		return render_str("showall.html", p = self)
 
 
 
 class AllPosts(Handler):
 		def get(self):
-			allposts = db.GqlQuery("SELECT * FROM Posts ORDER BY created DESC LIMIT 10")
+			#allposts = db.GqlQuery("SELECT * FROM Posts ORDER BY created DESC ") #LIMIT 10
+			allposts = Posts.all().order('-created')			
 			self.render('showall.html', allposts=allposts)
 
 
@@ -52,9 +63,10 @@ class NewPost(Handler):
 		content = self.request.get("content")
 		
 		if subject and content:
-			self.redirect("/blog/newpost/1234")
-			record = Posts(subject = subject, content = content)
-			record.put()
+			p = Posts(parent = blog_key(), subject = subject, content = content)
+			b_key = p.put()
+			#print b_key.id()
+			self.redirect("/blog/%s" % b_key.id())
 
 		else:
 			error = "subject and content, please!"
@@ -63,17 +75,30 @@ class NewPost(Handler):
 
 
 
-class Submitted(Handler):
+class Permalink(Handler):
+		def get(self, postId):
+			#lastpost = db.GqlQuery("SELECT * FROM Posts ORDER BY created DESC LIMIT 1")
+			key = db.Key.from_path('Posts', int(postId), parent=blog_key())
+			post = db.get(key)			
+			
+			if not post:
+				self.error(404)
+				return
+
+			self.render('submitted.html', lastpost=[post])
+
+
+
+
+class About(Handler):
 		def get(self):
-			lastpost = db.GqlQuery("SELECT * FROM Posts ORDER BY created DESC LIMIT 1")
-			self.render('submitted.html', lastpost=lastpost)
+			self.render('about.html')
 
 
 
 
-
-
-app = webapp2.WSGIApplication([('/blog', AllPosts),
-															 ('/blog/newpost',NewPost),
-															 ('/blog/newpost/1234', Submitted)							 
+app = webapp2.WSGIApplication([(r'/blog', AllPosts),
+															 (r'/blog/newpost',NewPost),
+															 (r'/blog/(\d+)', Permalink),
+															 (r'/blog/about', About)							 
 															], debug=True)
